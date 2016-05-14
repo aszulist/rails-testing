898482571c474d578ae7deed1dc64b45175f0d4b
Adam <aszulist@o2.pl>
2016-05-14 08:44:10 +0200

    Controller specs with authentication

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,13 +1,30 @@
 class AchievementsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :owner_only, only: [:edit, :update, :destroy]
+
+  def index
+    @achievements = Achievement.public_access
+  end
+
   def new
     @achievement = Achievement.new
+  end
+
+  def edit; end
+
+  def update
+    if @achievement.update_attributes(achievement_params)
+      redirect_to achievement_path(@achievement)
+    else
+      render :edit
+    end
   end
 
   def create
     @achievement = Achievement.new(achievement_params)
     if @achievement.save
-      redirect_to root_path, notice: 'Achievement has been created'
+      redirect_to achievement_url(@achievement), notice: 'Achievement has been created'
     else
       render :new
     end
@@ -15,12 +32,23 @@ class AchievementsController < ApplicationController
 
   def show
     @achievement = Achievement.find(params[:id])
-    @description = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@achievement.description)
+  end
+
+  def destroy
+    @achievement.destroy
+    redirect_to achievement_path
   end
 
   private
 
   def achievement_params
     params.require(:achievement).permit(:title, :description, :privacy, :cover_image, :featured)
+  end
+
+  def owner_only
+    @achievement = Achievement.find(params[:id])
+    if current_user != @achievement.user
+      redirect_to achievement_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'welcome#index'
 
-  resources :achievements, only: [:show, :new, :create]
+  resources :achievements
 end
